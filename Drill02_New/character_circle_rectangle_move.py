a42c9edd669de0c9f641c6bacf8e7b135538a7cc
jsw1211 <jswtiger@naver.com>
2023-09-21 10:02:31 +0900

    소년과 잔디 화면에 그리기

--- Drill02_New/character_circle_rectangle_move.py
+++ Drill02_New/character_circle_rectangle_move.py
@@ -7,17 +7,33 @@ character = load_image('character.png')
 
 def run_circle():
     print('CIRCLE')
+
+    #일단 그림을 그리자
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(400,90)
+    delay(1)
+
+
+
+
     pass
 
 
 def run_rectangle():
     print('RECTANGLE')
+
+
+
+
+
     pass
 
 
 while True:
     run_circle()
     run_rectangle()
+    break
     
 
 close_canvas()
