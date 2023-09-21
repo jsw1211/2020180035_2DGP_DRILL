276b5a7d980da2cc81e1efd8cd61f93d9a25418a
jsw1211 <jswtiger@naver.com>
2023-09-21 10:35:26 +0900

    소년 이동 함수 생성

--- Drill02_New/character_circle_rectangle_move.py
+++ Drill02_New/character_circle_rectangle_move.py
@@ -6,6 +6,13 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def render_frame(x, y):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+    delay(0.01)
+        
+
 def run_circle():
     print('CIRCLE')
 
@@ -16,22 +23,18 @@ def run_circle():
     for deg in range(0, 360, 1):
         x = cx + r * math.cos(math.radians(deg))
         y = cy + r * math.sin(math.radians(deg))
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.01)
+        render_frame(x, y)
 
 
 def run_rectangle():
     print('RECTANGLE')
 
     for x in range(50,750+1,10):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.01)
-        
+        render_frame(x, 90)
 
+#    for x in range(750,50-1,-10):
+#        render_frame(x, 90)
+        
 
 
 
@@ -39,8 +42,8 @@ def run_rectangle():
 
 
 while True:
+    run_circle()
     run_rectangle()
-    # run_circle()
     break
     
 
