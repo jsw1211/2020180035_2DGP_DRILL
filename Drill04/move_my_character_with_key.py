75795ff5944d7f8afd8d3f8d9bf3d179004f62f9
jsw1211 <jswtiger@naver.com>
2023-09-25 22:41:09 +0900

    Drill04 제출 (2020180035 정승원)

--- Drill04/move_my_character_with_key.py
+++ Drill04/move_my_character_with_key.py
@@ -71,21 +71,24 @@ while running:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     if motion == 0:
-        character.clip_draw(frame * 118, 390, 110, 160, x, y, 200, 200)
+        character.clip_draw(frame * 118, 390, 110, 160, x, y, 100, 100)
     elif motion == 1:
-        character.clip_composite_draw(frame * 118, 390, 110, 160, 0, 'h',x, y, 200, 200)
+        character.clip_composite_draw(frame * 118, 390, 110, 160, 0, 'h',x, y, 100, 100)
     elif motion == 2:
-        character.clip_draw(frame * 118, 390, 110, 160, x, y, 200, 200)
+        character.clip_draw(frame * 118, 390, 110, 160, x, y, 100, 100)
     elif motion == 3:
-        character.clip_draw(frame * 118, 390, 110, 160, x, y, 200, 200)
+        character.clip_draw(frame * 118, 390, 110, 160, x, y, 100, 100)
     elif motion == 4:
-        character.clip_draw(580, 50, 250, 100, x, y, 200, 200)
+        character.clip_draw(580, 50, 250, 100, x, y, 100, 100)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 6
     if motion != 4:
-        x += dir_x * 5
-        y += dir_y * 5
+        x += dir_x * 20
+        y += dir_y * 20
+
+    x = clamp(50, x, TUK_WIDTH - 50)
+    y = clamp(180, y, TUK_HEIGHT - 130)
     delay(0.05)
 
 close_canvas()
