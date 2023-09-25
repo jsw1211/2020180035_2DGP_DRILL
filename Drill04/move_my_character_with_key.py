dc756649e31a20b42b7d9dcab9520304f6d31f42
jsw1211 <jswtiger@naver.com>
2023-09-25 21:54:05 +0900

    캐릭터 좌우 방향 전환 구현

--- Drill04/move_my_character_with_key.py
+++ Drill04/move_my_character_with_key.py
@@ -8,6 +8,7 @@ character = load_image('character.png')
 def handle_events():
     global running, dir_x, dir_y
     global x, y
+    global motion
 
     events = get_events()
     for event in events:
@@ -16,23 +17,43 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 dir_x += 1
+                motion = 0               
             elif event.key == SDLK_LEFT:
                 dir_x -= 1
+                motion = 1
             elif event.key == SDLK_UP:
                 dir_y += 1
+                if motion == 2:
+                    motion = 0
+                elif motion == 3:
+                    motion = 1
             elif event.key == SDLK_DOWN:
                 dir_y -= 1
+                if motion == 2:
+                    motion = 0
+                elif motion == 3:
+                    motion = 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
                 dir_x -= 1
+                motion = 0
             elif event.key == SDLK_LEFT:
                 dir_x += 1
+                motion = 1
             elif event.key == SDLK_UP:
                 dir_y -= 1
+                if motion == 2:
+                    motion = 0
+                elif motion == 3:
+                    motion = 1
             elif event.key == SDLK_DOWN:
                 dir_y += 1
+                if motion == 2:
+                    motion = 0
+                elif motion == 3:
+                    motion = 1
 
 running = True
 x = 1280 // 2
@@ -40,11 +61,19 @@ y = 1024 // 2
 frame = 0
 dir_x = 0
 dir_y = 0
+motion = 0
 
 while running:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 118, 390, 110, 160, x, y)
+    if motion == 0:
+        character.clip_draw(frame * 118, 390, 110, 160, x, y, 200, 200)
+    elif motion == 1:
+        character.clip_composite_draw(frame * 118, 390, 110, 160, 0, 'h',x, y, 200, 200)
+    elif motion == 2:
+        character.clip_draw(frame * 118, 390, 110, 160, x, y, 200, 200)
+    elif motion == 3:
+        character.clip_draw(frame * 118, 390, 110, 160, x, y, 200, 200)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 6
