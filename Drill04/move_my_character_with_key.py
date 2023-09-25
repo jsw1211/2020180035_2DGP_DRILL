0a31d3bb4f9579276c8fe1b2a7490852eea7f490
jsw1211 <jswtiger@naver.com>
2023-09-25 20:26:20 +0900

    캐릭터 x축 이동 구현

--- Drill04/move_my_character_with_key.py
+++ Drill04/move_my_character_with_key.py
@@ -5,10 +5,41 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('character.png')
 
+def handle_events():
+    global running, dir_x
+    global x
 
-clear_canvas()
-tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-update_canvas()
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir_x += 1
+            elif event.key == SDLK_LEFT:
+                dir_x -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir_x -= 1
+            elif event.key == SDLK_LEFT:
+                dir_x += 1
+
+running = True
+x = 1280 // 2
+frame = 0
+dir_x = 0
+
+while running:
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 118, 390, 110, 160, x, 500)
+    update_canvas()
+    handle_events()
+    frame = (frame + 1) % 6
+    x += dir_x * 5
+    delay(0.05)
 
 close_canvas()
 
