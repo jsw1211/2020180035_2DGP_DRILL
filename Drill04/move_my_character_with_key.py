e591e8e90bfb103bf0b1f085b103ece49787939a
jsw1211 <jswtiger@naver.com>
2023-09-25 20:31:09 +0900

    캐릭터 y축 이동 구현

--- Drill04/move_my_character_with_key.py
+++ Drill04/move_my_character_with_key.py
@@ -6,8 +6,8 @@ tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('character.png')
 
 def handle_events():
-    global running, dir_x
-    global x
+    global running, dir_x, dir_y
+    global x, y
 
     events = get_events()
     for event in events:
@@ -18,6 +18,10 @@ def handle_events():
                 dir_x += 1
             elif event.key == SDLK_LEFT:
                 dir_x -= 1
+            elif event.key == SDLK_UP:
+                dir_y += 1
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
@@ -25,20 +29,27 @@ def handle_events():
                 dir_x -= 1
             elif event.key == SDLK_LEFT:
                 dir_x += 1
+            elif event.key == SDLK_UP:
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
 
 running = True
 x = 1280 // 2
+y = 1024 // 2
 frame = 0
 dir_x = 0
+dir_y = 0
 
 while running:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 118, 390, 110, 160, x, 500)
+    character.clip_draw(frame * 118, 390, 110, 160, x, y)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 6
     x += dir_x * 5
+    y += dir_y * 5
     delay(0.05)
 
 close_canvas()
