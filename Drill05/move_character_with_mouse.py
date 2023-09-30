b59c1c75c3b2ead0359c6aaea34036f9c2daade5
jsw1211 <jswtiger@naver.com>
2023-09-30 20:35:54 +0900

    이동 버그 수정

--- Drill05/move_character_with_mouse.py
+++ Drill05/move_character_with_mouse.py
@@ -16,8 +16,6 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -44,11 +42,14 @@ def character_move():
     for i in range(0, 100+1, 1):
         t = i / 100
         x = (1 - t) * x + t * arrow_x
-        y = (1 - t) * x + t * arrow_y
+        y = (1 - t) * y + t * arrow_y
         clear_canvas()
         TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
         arrow.draw(arrow_x, arrow_y)
-        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        if (x < arrow_x):
+            character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        else:
+            character.clip_composite_draw(frame * 100, 100 * 1, 100, 100, 0, 'h', x, y, 100, 100)
         update_canvas()
         frame = (frame + 1) % 8
         delay(0.01)
