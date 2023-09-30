12432a55b807f399d9bdac212786a6a813c2cd3b
jsw1211 <jswtiger@naver.com>
2023-10-01 01:14:54 +0900

    arrow 마우스로 움직이게 설정

--- Drill06/move_character_with_mouse.py
+++ Drill06/move_character_with_mouse.py
@@ -5,17 +5,19 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
+arrow = load_image('hand_arrow.png')
 
 
 def handle_events():
     global running
     global x, y
+    global arrow_x, arrow_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
+            arrow_x, arrow_y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -24,6 +26,7 @@ def handle_events():
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+arrow_x, arrow_y = x, y
 frame = 0
 hide_cursor()
 
@@ -31,6 +34,7 @@ while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    arrow.draw(arrow_x, arrow_y)
     update_canvas()
     frame = (frame + 1) % 8
 
