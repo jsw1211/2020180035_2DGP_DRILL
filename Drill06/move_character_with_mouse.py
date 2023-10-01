cfd515a409a8ca2925a843bead30b79283bb97de
jsw1211 <jswtiger@naver.com>
2023-10-01 16:04:26 +0900

    마우스 찍히고 움직이기 구현

--- Drill06/move_character_with_mouse.py
+++ Drill06/move_character_with_mouse.py
@@ -12,6 +12,7 @@ def handle_events():
     global running
     global x, y
     global arrow_x, arrow_y
+    global arrow_cx, arrow_cy
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -21,7 +22,7 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
         elif event.type == SDL_MOUSEBUTTONDOWN:
-            arrow.draw(arrow_x, arrow_y)
+            arrow_cx, arrow_cy = arrow_x, arrow_y
     pass
 
 def character_move():
@@ -29,15 +30,18 @@ def character_move():
     global c_x, c_y
     global x, y
     global frame
+    global arrow_cx, arrow_cy
     c_x, c_y = x, y
+    arrow_cx, arrow_cy = arrow_x, arrow_y
     for i in range(0, 100+1, 1):
         t = i / 100
-        x = (1 - t) * c_x + t * arrow_x
-        y = (1 - t) * c_x + t * arrow_y
+        x = (1 - t) * c_x + t * arrow_cx
+        y = (1 - t) * c_y + t * arrow_cy
         clear_canvas()
         TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+        arrow.draw(arrow_cx, arrow_cy)
         arrow.draw(arrow_x, arrow_y)
-        if (x < arrow_x):
+        if (x < arrow_cx):
             character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
         else:
             character.clip_composite_draw(frame * 100, 100 * 1, 100, 100, 0, 'h', x, y, 100, 100)
@@ -45,18 +49,19 @@ def character_move():
         update_canvas()
         frame = (frame + 1) % 8
         delay(0.01)
+        handle_events()
 
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 arrow_x, arrow_y = x, y
+arrow_cx, arrow_cy = arrow_x, arrow_y
 frame = 0
 hide_cursor()
 
 while running:
     character_move()
 
-    handle_events()
 
 close_canvas()
 
