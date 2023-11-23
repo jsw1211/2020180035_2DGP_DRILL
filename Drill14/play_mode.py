55cfffe0395a3ad44c3883f972dac8795296a997
jsw1211 <jswtiger@naver.com>
2023-11-23 10:40:29 +0900

    수업 내용

--- Drill14/play_mode.py
+++ Drill14/play_mode.py
@@ -10,6 +10,7 @@ import server
 from boy import Boy
 
 # fill here
+from background import FixedBackground as Background
 
 
 
@@ -28,6 +29,12 @@ def handle_events():
 
 def init():
     # fill here
+    server.background = Background()
+    game_world.add_object(server.background, 0)
+
+    server.boy = Boy()
+    game_world.add_object(server.boy, 1)
+    server.boy.set_background(server.background)
     pass
 
 def finish():
